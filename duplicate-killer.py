d35860547658f9df761dc317fd89b9ca8c7dc80e
A-Coom <allenwoolman@gmail.com>
2023-02-21 21:37:00 -0600

    20230221 - Intermediate push

--- duplicate-killer.py
+++ duplicate-killer.py
@@ -1,30 +1,49 @@
+from scraping_utils.scraping_utils import compute_file_hashes
 import os
 import shutil
+import hashlib
 from tempfile import TemporaryDirectory
-from hashlib import md5
 from sys import stdout, argv
-
-
-EXTS = ['jpg', 'png', 'gif', 'jpeg',
-        'mp4', 'm4v', 'mkv', 'webm',
-        'mp3', 'wav']
 
 
 """
 Driver function. Save only unique files from a source directory into a destination directory.
 @param src - Source to query.
-@param dst - Destination free of duplicates. If None, use a tmp directory and overwrite the source.
+@param dst - Destination free of duplicates. If equal, a temporary directory is used.
+@param rename - Boolean to rename the files to the computed hash sum.
 """
-def main(src, dst):
-    # If no destination was specified, take note and create a tmp directory
-    is_tmp = False
-    if(dst is None):
-        is_tmp = True
+def main(src, dst, rename):
+    # Remove any the trailing separator from the source and destination
+    if(src.endswith(os.sep)):
+        src = src[:-1]
+    if(dst.endswith(os.sep)):
+        dst = dst[:-1]
+    
+    # If the source and destination are equal, take note and use a tmp directory.
+    is_tmp = (src == dst)
+    if(is_tmp):
         dst = TemporaryDirectory().name
         os.mkdir(dst)
-    stdout.write('Removing duplicates from (%s) and storing in (%s).\n' % (src, dst))
+        stdout.write('[main] INFO: Removing duplicates from (%s) with intermediate directory (%s).\n' % (src, dst))
+    else:
+        stdout.write('[main] INFO: Removing duplicates from (%s) and storing in (%s).\n' % (src, dst))
     
+    # Get the hash of all files and their paths
+    hashes = compute_file_hashes(src, recurse=True)
     
+    # Iterate the hashes, saving the files into the destination
+    for hash in hashes:
+        file = hashes[hash].replace(src, dst)
+        tree = file.split(os.sep)
+        if(rename):
+            old_name = tree[-1]
+            ext = old_name.split('.')[-1]
+            file = file.replace(old_name, (hash + '.' + ext))
+        sub = os.sep.join(tree[:-1])
+        if(not os.path.isdir(sub)):
+            stdout.write('[main] INFO: Creating sub directory (%s).\n' % (sub))
+            os.makedirs(sub)
+        shutil.copyfile(hashes[hash], file)
     
     # If no destination was specified, replace the source with the tmp destination
     if(is_tmp):
@@ -37,15 +56,14 @@ Entrypoint
 """
 if __name__ == '__main__':
     stdout.write('\n')
-    if(len(argv) < 2):
-        stdout.write('USAGE: %s <src_dir> ?dst_dir?\n' % (argv[0]))
+    if(len(argv) < 4):
+        stdout.write('USAGE: %s <src_dir> <dst_dir> <rename>\n' % (argv[0]))
     else:
         if(not os.path.isdir(argv[1])):
             stdout.write('Not a directory (%s).\n' % (argv[1]))
         else:
-            if(len(argv) < 3):
-                argv.append(None)
-            elif(not os.path.isdir(argv[2])):
+            if(not os.path.isdir(argv[2])):
                 os.mkdir(argv[2])
-            main(argv[1], argv[2])
+            argv[3] = argv[3].lower()[0]
+            main(argv[1], argv[2], (argv[3] == 't' or argv[3] == 'y'))
     stdout.write('\n')
