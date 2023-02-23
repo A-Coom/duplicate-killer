d6811f2ad2929bbb61d881170301b8c6621e7b80
A-Coom <allenwoolman@gmail.com>
2023-02-23 14:50:34 -0600

    20230223 - Added move (no copy) option.

--- duplicate-killer.py
+++ duplicate-killer.py
@@ -11,8 +11,9 @@ Driver function. Save only unique files from a source directory into a destinati
 @param src - Source to query.
 @param dst - Destination free of duplicates. If equal, a temporary directory is used.
 @param rename - Boolean to rename the files to the computed hash sum.
+@param move - Boolean to move the files instead of copying them. Much faster, but interrupts could cause corruption.
 """
-def main(src, dst, rename):
+def main(src, dst, rename, move):
     # Remove any the trailing separator from the source and destination
     if(src.endswith(os.sep)):
         src = src[:-1]
@@ -30,6 +31,7 @@ def main(src, dst, rename):
     
     # Get the hash of all files and their paths
     hashes = compute_file_hashes(src, recurse=True)
+    stdout.write('[main] INFO: Computed (%d) unique hashes.\n' % (len(hashes)))
     
     # Iterate the hashes, saving the files into the destination
     for hash in hashes:
@@ -43,7 +45,12 @@ def main(src, dst, rename):
         if(not os.path.isdir(sub)):
             stdout.write('[main] INFO: Creating sub directory (%s).\n' % (sub))
             os.makedirs(sub)
-        shutil.copyfile(hashes[hash], file)
+        if(move):
+            stdout.write('[main] INFO: Moving (%s) to (%s).\n' % (hashes[hash], file))
+            shutil.move(hashes[hash], file)
+        else:
+            stdout.write('[main] INFO: Copying (%s) to (%s).\n' % (hashes[hash], file))
+            shutil.copyfile(hashes[hash], file)
     
     # If no destination was specified, replace the source with the tmp destination
     if(is_tmp):
@@ -56,8 +63,8 @@ Entrypoint
 """
 if __name__ == '__main__':
     stdout.write('\n')
-    if(len(argv) < 4):
-        stdout.write('USAGE: %s <src_dir> <dst_dir> <rename>\n' % (argv[0]))
+    if(len(argv) < 5):
+        stdout.write('USAGE: %s <src_dir> <dst_dir> <rename> <do_not_copy>\n' % (argv[0]))
     else:
         if(not os.path.isdir(argv[1])):
             stdout.write('Not a directory (%s).\n' % (argv[1]))
@@ -65,5 +72,6 @@ if __name__ == '__main__':
             if(not os.path.isdir(argv[2])):
                 os.mkdir(argv[2])
             argv[3] = argv[3].lower()[0]
-            main(argv[1], argv[2], (argv[3] == 't' or argv[3] == 'y'))
+            argv[4] = argv[4].lower()[0]
+            main(argv[1], argv[2], (argv[3] == 't' or argv[3] == 'y'), (argv[4] == 't' or argv[4] == 'y'))
     stdout.write('\n')
